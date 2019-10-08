674d10a3978dc2c63e19c488eba139ebd0b28f5f
ElizabethAChen <elizabethchen@csu.fullerton.edu>
2019-10-07 19:41:03 -0700

    A placeholder for the future project and any completions that occur along the way

--- app/src/main/java/com/kingbart/radicallydifferent/MainActivity.kt
+++ app/src/main/java/com/kingbart/radicallydifferent/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.kingbart.radicallydifferent
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ic_logo.startAnimation(AnimationUtils.loadAnimation(this,R.anim.splash_in))
+        Handler().postDelayed({
+            ic_logo.startAnimation(AnimationUtils.loadAnimation(this,R.anim.splash_out))
+            Handler().postDelayed({
+                ic_logo.visibility = View.GONE
+                startActivity(Intent(this, AppIntro::class.java))
+                finish()
+            },500)
+        },1500)
+
     }
 }
